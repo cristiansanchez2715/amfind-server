59a5cfff3eaddb0468fda26b6dc0f562ce389829
cristiansanchez2715 <cristiansanchez2715@gmail.com>
2024-03-20 19:54:13 -0500

    subiendo server

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.PORT = process.env.PORT || 3000;
+exports.DB_HOST = process.env.DB_HOST || 'localhost';
+exports.DB_USER = process.env.DB_USER || 'root';
+exports.DB_PASSWORD = process.env.DB_PASSWORD || 'spizamarillo2715';
+exports.DB_NAME = process.env.DB_NAME || 'usersdb';
+exports.DB_PORT = process.env.DB_PORT || 3306;
